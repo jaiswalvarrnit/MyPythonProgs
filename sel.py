95ba51b4e58b11d0b658e2faddbbea3795a6e9cc
Varrnit <jaiswalvarrnit@gmail.com>
2022-06-23 21:16:25 +0530

    Added Files

---
+++ sel.py
@@ -0,0 +1,5 @@
+from selenium import webdriver
+# from webdriver_manager.chrome import ChromeDriverManager
+# driver = webdriver.Chrome(ChromeDriverManager().install())
+driver = webdriver.Chrome()
+driver.get("https://leapsapp.analyttica.com/auth")
